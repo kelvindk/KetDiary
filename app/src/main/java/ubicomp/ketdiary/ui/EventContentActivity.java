daae31932ba72f3653990762879c6d2a7c9f993f
Li Cheng Yuan <kelvindk@Kelvin-Li.local>
2016-06-10 17:05:48 +0800

    no message

--- app/src/main/java/ubicomp/ketdiary/ui/EventContentActivity.java
+++ app/src/main/java/ubicomp/ketdiary/ui/EventContentActivity.java
@@ -2,6 +2,9 @@ package ubicomp.ketdiary.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.WindowManager;
 
 import ubicomp.ketdiary.R;
 
@@ -14,6 +17,21 @@ public class EventContentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_content);
+
+        // Set full screen.
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN );
+
+        // Enable toolbar on create event activity with back button on the top left.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_event_content_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
 
     }
 
