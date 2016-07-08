acd81a9fca353900cdf5482af4e888fcd0fb5984
Li Cheng Yuan <kelvindk@Kelvin-Li.local>
2016-07-08 08:55:19 +0800

    no message

--- app/src/main/java/ubicomp/ketdiary/fragments/event/EventIIncompleteActivity.java
+++ app/src/main/java/ubicomp/ketdiary/fragments/event/EventIIncompleteActivity.java
@@ -1,7 +1,21 @@
 package ubicomp.ketdiary.fragments.event;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ubicomp.ketdiary.R;
+
 /**
  * Created by kelvindk on 16/7/7.
  */
-public class EventIIncompleteActivity {
+public class EventIIncompleteActivity extends AppCompatActivity {
+
+    public static final String TAG = "EventIIncompleteActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+    }
+
 }
